9be9930fdd082bf758db7808a0d2378a691a3e0a
dahmanp <dahmanp@my.erau.edu>
2023-04-05 17:57:13 -0700

    add project

---
+++ Terrible Tweeters/Assets/Scripts/Monster.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+
+    bool _hasDied;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(ShouldDieFromCollision(collision))
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    bool ShouldDieFromCollision(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if(_hasDied)
+        {
+            return false;
+        }
+        
+        if (bird != null)
+        {
+            return true;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator Die()
+    {
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+    }
+}
